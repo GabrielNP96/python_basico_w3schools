7113d5659307d869c09b6c4a7cc1e483046b080c
GabrielNP96 <gabrielnp1996@gmail.com>
2025-06-18 09:57:31 -0300

    Aprendendo sobre variaveis

---
+++ 01_basico/02_comentarios.py
@@ -0,0 +1,4 @@
+#isso é um comentario
+"""
+Isso também é um comentario
+"""
---
+++ 01_basico/03_variaveis.py
@@ -0,0 +1,14 @@
+#variaveis são usadas para guardar dados
+name = "Roger"
+print(name)
+#A variavel name é igual a Roger
+name = 3
+print(name)
+#e no python não podem ser tipadas de forma tradicional
+age = int(15)
+print(age)
+age = "Biscoito"
+print(age)
+#para descobri o tipo de uma variavel é so usar type()
+print(type(age))
+print(type(name))
